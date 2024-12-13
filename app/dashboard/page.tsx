77a2ca2ba7ed2435e6ebdfedfd69e9049100585f
akifcan <akfkara97@gmail.com>
2024-12-14 00:18:14 +0300

    build(zustand): zustand setup

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+'use client'
+import useBearStore from '@/store/bears/bears.store'
+
 export default function Dashboard() {
-  return <div>ok</div>;
+  const { bears, resetBears } = useBearStore()
+
+  return (
+    <div>
+      <p>{bears}</p>
+      <button onClick={resetBears}>reset</button>
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link";
+'use client'
+import useBearStore from '@/store/bears/bears.store'
+import Link from 'next/link'
 
 export default function Home() {
+  const { bears, increase } = useBearStore()
+
   return (
     <div>
-      <Link href={"/dashboard"}>dashboard</Link>
+      <Link href={'/dashboard'}>dashboard</Link>
+      <p>{bears}</p>
+      <button onClick={increase}>inc</button>
     </div>
-  );
+  )
 }
